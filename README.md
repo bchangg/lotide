238af2e08b9e20e3aa828d3bb4b7a10b35c46389
Brian Chang <changbhc@gmail.com>
2019-06-24 21:56:18 +0000

    Added README.md, completed assertEqual.js

---
+++ README.md
@@ -0,0 +1,4 @@
+# Lotide Project
+
+## Description
+Lighthouse Labs project 1, basically implementing a new library
---
+++ assertEqual.js
@@ -0,0 +1,12 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual !== expected) {
+    console.log("\u274C Assertion Failed: " + actual + " !== " + expected);
+  } else {
+    console.log("\u2705 Assertion Passed: " + actual + " === " + expected);
+  }
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
