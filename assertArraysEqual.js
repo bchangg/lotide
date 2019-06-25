1e2d427e7817db4886df64e3726a3db76705b54d
Brian Chang <changbhc@gmail.com>
2019-06-25 23:04:49 +0000

    Removed test cases inside assertArraysEqual.js

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -13,7 +13,3 @@ const eqArrays = function(array1, array2) {
 const assertArraysEqual = function(arr1, arr2) {
   console.log(eqArrays(arr1, arr2));
 };
-
-assertArraysEqual([1,2],[1,2]);
-assertArraysEqual([1,2],[1,3]);
-assertArraysEqual([1,2],["1",2]);
