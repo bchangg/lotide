526cfe422e3f88cc3b80c4fcaedb36f6343484cc
Brian Chang <changbhc@gmail.com>
2019-06-25 22:06:05 +0000

    Completed eqArrays.js

---
+++ eqArrays.js
@@ -0,0 +1,25 @@
+const assertEqual = function(actual, expected) {
+  if (actual !== expected) {
+    console.log(`\u274C\u274C\u274C Assertion Failed: ${actual} !== ${expected}`);
+  } else {
+    console.log(`\u2705\u2705\u2705 Assertion Passed: ${actual} === ${expected}`);
+  }
+};
+
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  for(let i = 0; i < array1.length; i++) {
+    if(array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => PASS
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => FAIL
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => PASS
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => FAIL
