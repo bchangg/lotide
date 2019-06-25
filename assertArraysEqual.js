eb0bdb05932e838cc5328a3657cd84d1520c5ca2
Brian Chang <changbhc@gmail.com>
2019-06-25 23:04:22 +0000

    Completed assertArraysEqual.js

---
+++ assertArraysEqual.js
@@ -0,0 +1,19 @@
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(arr1, arr2) {
+  console.log(eqArrays(arr1, arr2));
+};
+
+assertArraysEqual([1,2],[1,2]);
+assertArraysEqual([1,2],[1,3]);
+assertArraysEqual([1,2],["1",2]);
