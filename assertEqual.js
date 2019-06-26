f340ca037460ed34076eb9519e6fb0f2328b291d
Brian Chang <changbhc@gmail.com>
2019-06-26 22:44:00 +0000

    Updated function description

--- assertEqual.js
+++ assertEqual.js
@@ -1,5 +1,6 @@
 // @desc: Assertion function to test two values that are not arrays
-// @param: Two non-object values
+// @param: value
+// @param: value
 // @return: Nothing. Funciton is a passive function, and only logs the results to the console.
 const assertEqual = function(actual, expected) {
   if (actual !== expected) {
