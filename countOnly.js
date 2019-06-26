94ec99225afa0f75e7d4040afee9e838d11669dc
Brian Chang <changbhc@gmail.com>
2019-06-26 16:51:17 +0000

    Created countOnly.js

---
+++ countOnly.js
@@ -0,0 +1,8 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual !== expected) {
+    console.log(`\u274C Assertion Failed: ${actual} !== ${expected}`);
+  } else {
+    console.log(`\u2705 Assertion Passed: ${actual} === ${expected}`);
+  }
+};
