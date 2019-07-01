7423c0d4445a6d3e59ab51b2bc6775aee00e34ed
Brian Chang <changbhc@gmail.com>
2019-07-01 20:58:46 +0000

    Refactored tail.js and added tailTest.js in ./test

--- tail.js
+++ tail.js
@@ -11,13 +11,4 @@ const tail = function(array) {
   return array.slice(1);
 };
 
-// TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-
-assertEqual(tail([]), [2]);
-
-// Test Case: Check the original array
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
+module.exports = tail;
