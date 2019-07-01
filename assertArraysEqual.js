49750194bfc184c6b98d00c940719a8d156a267d
Brian Chang <changbhc@gmail.com>
2019-07-01 21:17:14 +0000

    Refactored assertArraysEqual.js && moved test ode for assertArraysEqual.js into assertArraysEqualTest.js

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,15 +1,4 @@
-const eqArrays = function(array1, array2) {
-  if (array1.length !== array2.length) {
-    return false;
-  }
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-
+const eqArrays = require("./eqArrays");
 // @desc: Assertion function to check if two arrays are equal, used in tests. Prints the results to
 //        the console.
 // @param: 2 arrays
@@ -22,3 +11,5 @@ const assertArraysEqual = function(actual, expected) {
     console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
   }
 };
+
+module.exports = assertArraysEqual;
---
+++ test/assertArraysEqualTest.js
@@ -0,0 +1,3 @@
+const assertArraysEqual = require("../assertArraysEqual");
+
+console.log(assertArraysEqual([2,3], [2,3]));
